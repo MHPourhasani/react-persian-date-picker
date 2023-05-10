a2a5c2f61bf6a90a89b40e1059a2b75cbd8d52a5
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-05-10 11:49:15 +0430

    created Calendar component

---
+++ src/persianDatePickerReact/components/Calendar/CalendarInterface.ts
@@ -0,0 +1,32 @@
+import { Calendar, DateType, Locale } from "react-date-object";
+
+export interface CalendarProps {
+  value: any;
+  calendar: Calendar;
+  locale: Locale;
+  format: string;
+  onChange: (a: any) => any;
+  minDate: any;
+  maxDate: any;
+  digits: string[];
+  calendarClassName: string;
+  dayClassName: string;
+  todayClassName: string;
+  selectedDayClassName: string;
+  disabledDayClassName: string;
+}
+
+export interface StateType {
+  date: any;
+  selectedDate: any;
+  focused: any;
+  mustSortDates: any;
+  today: DateType;
+  minDate: any;
+  maxDate: any;
+  year: number;
+  mustShowYearPicker: boolean;
+  calendar: Calendar;
+  locale: Locale;
+  format: string;
+}
