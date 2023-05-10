1e73b8bbeb9fe2b06a430e07c48fcb248c3695b2
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-05-10 11:55:59 +0430

    imported DatePicker and MobileDatePicker components to main.tsx

---
+++ src/persianDatePickerReact/main.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { DatePickerProps } from "./components/DatePicker/DatePickerInterface";
+import DatePicker from "./components/DatePicker/DatePicker";
+import MobileDatePicker from "./components/Mobile/MobileDatePicker/MobileDatePicker";
+import { MobileDatePickerProps } from "./components/Mobile/MobileDatePicker/MobileDatePickerInterface";
+import "../index.css";
+
+export const DatePickerContainer = (props: DatePickerProps) => {
+  return (
+    <DatePicker
+      value={props.value}
+      onChange={props.onChange}
+      maxDate={props.maxDate}
+      inputClassName={props.inputClassName}
+      calendarClassName={props.calendarClassName}
+      dayClassName={props.dayClassName}
+      todayClassName={props.todayClassName}
+      selectedDayClassName={props.selectedDayClassName}
+      disabledDayClassName={props.disabledDayClassName}
+    />
+  );
+};
+
+export const MobileDatePickerContainer = (props: MobileDatePickerProps) => {
+  <MobileDatePicker
+    value={props.value}
+    onChange={props.onChange}
+    maxDate={props.maxDate}
+    inputClassName={props.inputClassName}
+    calendarClassName={props.calendarClassName}
+    dayClassName={props.dayClassName}
+    todayClassName={props.todayClassName}
+    selectedDayClassName={props.selectedDayClassName}
+    disabledDayClassName={props.disabledDayClassName}
+  />;
+};
