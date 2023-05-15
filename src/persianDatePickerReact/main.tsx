b46c872f72f1967710ad4906c1d2c1d86822d649
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-05-15 15:21:29 +0330

    updated DatePickerContainer and MobileDatePickerContainer in main.tsx

--- src/persianDatePickerReact/main.tsx
+++ src/persianDatePickerReact/main.tsx
@@ -9,11 +9,10 @@ export const DatePickerContainer = (props: DatePickerProps) => {
   return (
     <DatePicker
       value={props.value}
-      onChange={props.onChange}
       minDate={props.minDate}
       maxDate={props.maxDate}
       timePicker={props.timePicker}
-      inputClassName={props.inputClassName}
+      inputContainerClassName={props.inputContainerClassName}
       calendarClassName={props.calendarClassName}
       dayClassName={props.dayClassName}
       todayClassName={props.todayClassName}
@@ -27,11 +26,10 @@ export const MobileDatePickerContainer = (props: MobileDatePickerProps) => {
   return (
     <MobileDatePicker
       value={props.value}
-      onChange={props.onChange}
       minDate={props.minDate}
       maxDate={props.maxDate}
       timePicker={props.timePicker}
-      inputClassName={props.inputClassName}
+      inputContainerClassName={props.inputContainerClassName}
       calendarClassName={props.calendarClassName}
       dayClassName={props.dayClassName}
       todayClassName={props.todayClassName}
