121ae53c37f26d5c06921da95c62e7a69884f0b5
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-06-07 08:57:17 +0330

    updated MobileDatePicker styles

--- src/persianDatePickerReact/components/Mobile/MobileDatePicker/MobileDatePicker.tsx
+++ src/persianDatePickerReact/components/Mobile/MobileDatePicker/MobileDatePicker.tsx
@@ -17,138 +17,134 @@ import CalendarIcon from "../../../assets/svg/CalendarIcon";
 import { MobileDatePickerProps } from "./MobileDatePickerInterface";
 
 const MobileDatePicker: React.FC<MobileDatePickerProps> = ({
-  value,
-  onChange,
-  minDate,
-  maxDate,
-  timePicker = true,
-  readOnly = false,
-  inputPlaceholder,
-  inputContainerClassName,
-  calendarClassName,
-  dayClassName,
-  todayClassName,
-  selectedDayClassName,
-  disabledDayClassName,
+	value,
+	onChange,
+	minDate,
+	maxDate,
+	timePicker = true,
+	readOnly = false,
+	inputPlaceholder,
+	inputContainerClassName,
+	calendarClassName,
+	dayClassName,
+	todayClassName,
+	selectedDayClassName,
+	disabledDayClassName,
 }) => {
-  const [date, setDate] = useState(),
-    [stringDate, setStringDate] = useState<string>("");
+	const [date, setDate] = useState(),
+		[stringDate, setStringDate] = useState<string>("");
 
-  const inputRef = useRef<any>();
+	const inputRef = useRef<any>();
 
-  const calendar = persian,
-    locale = persian_fa,
-    digits = locale.digits;
+	const calendar = persian,
+		locale = persian_fa,
+		digits = locale.digits;
 
-  let format = "YYYY/MM/DD HH:mm";
+	let format = "YYYY/MM/DD HH:mm";
 
-  if (!timePicker) {
-    format = "YYYY/MM/DD";
-  }
+	if (!timePicker) {
+		format = "YYYY/MM/DD";
+	}
 
-  useEffect(() => {
-    let date = value;
+	useEffect(() => {
+		let date = value;
 
-    const checkDate = (date: any) => {
-      if (!date) return;
-      if (!(date instanceof DateObject))
-        date = new DateObject({ date, calendar, locale, format });
+		const checkDate = (date: any) => {
+			if (!date) return;
+			if (!(date instanceof DateObject)) date = new DateObject({ date, calendar, locale, format });
 
-      date.set({ locale, format });
+			date.set({ locale, format });
 
-      return date;
-    };
+			return date;
+		};
 
-    date = checkDate(date);
+		date = checkDate(date);
 
-    setStringDate(date ? date.format() : "");
-    setDate(date);
-  }, [value, calendar, locale, format, timePicker]);
+		setStringDate(date ? date.format() : "");
+		setDate(date);
+	}, [value, calendar, locale, format, timePicker]);
 
-  const handleChange = (date: any) => {
-    setDate(date);
-    setStringDate(date ? date.format() : "");
-  };
+	const handleChange = (date: any) => {
+		setDate(date);
+		setStringDate(date ? date.format() : "");
+	};
 
-  return (
-    <div className="h-auto">
-      <Popover className="flex w-full flex-col overflow-y-auto">
-        <>
-          <Popover.Button
-            className={`outline-none bg-white z-10 h-12 border-1 border-secondary300 focus:border-primary rounded-lg cursor-pointer pl-2 ${inputContainerClassName}`}
-          >
-            <div dir="ltr" className="relative flex h-full w-full">
-              <input
-                ref={inputRef}
-                id="date-picker-input"
-                readOnly
-                type="text"
-                value={stringDate}
-                data-time-stamp={toTimeStamp(date, timePicker)}
-                placeholder={inputPlaceholder}
-                className={readOnly ? "cursor-not-allowed" : "cursor-pointer"}
-              />
+	return (
+		<div className="h-auto font-iranyekan sm:hidden">
+			<Popover className="flex w-full flex-col overflow-y-auto">
+				<>
+					<Popover.Button
+						className={`outline-none bg-white z-10 h-12 border-1 border-secondary300 focus:border-primary rounded-lg cursor-pointer pl-2 ${inputContainerClassName}`}
+					>
+						<div dir={stringDate ? "ltr" : "rtl"} className="relative flex h-full w-full">
+							<input
+								ref={inputRef}
+								id="date-picker-input"
+								readOnly
+								type="text"
+								value={stringDate ? stringDate : null}
+								data-time-stamp={toTimeStamp(date, timePicker)}
+								placeholder={inputPlaceholder}
+								className={readOnly ? "cursor-not-allowed" : "cursor-pointer"}
+							/>
 
-              <label
-                htmlFor="date-picker-input"
-                className="bg-[#D9D9D9] absolute right-0 flex h-full cursor-pointer items-center justify-center rounded-r-lg px-4"
-              >
-                <CalendarIcon className="!fill-[#666666]" />
-              </label>
-            </div>
-          </Popover.Button>
+							<label
+								htmlFor="date-picker-input"
+								className="bg-[#D9D9D9] absolute right-0 flex h-full cursor-pointer items-center justify-center rounded-r-lg px-4"
+							>
+								<CalendarIcon className="!fill-[#666666]" />
+							</label>
+						</div>
+					</Popover.Button>
 
-          {!readOnly && (
-            <>
-              <Transition.Child
-                as={Fragment}
-                enter="ease-in-out"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in-out"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <div className="fixed inset-0 w-screen bg-gray-900 bg-opacity-50" />
-              </Transition.Child>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-200"
-                enterFrom="opacity-0 translate-y-full duration-300"
-                enterTo="opacity-100 h-auto translate-y-0"
-                leave="transition ease-in duration-150"
-                leaveFrom="opacity-100 translate-y-0"
-                leaveTo="opacity-0 translate-y-full duration-300"
-              >
-                <Popover.Panel
-                  dir="rtl"
-                  className="absolute bottom-0 w-screen z-10 rounded-t-3xl bg-white p-4"
-                >
-                  <Calendar
-                    value={date}
-                    changeHandler={handleChange}
-                    propsOnChange={onChange}
-                    calendar={calendar}
-                    locale={locale}
-                    format={format}
-                    digits={digits}
-                    minDate={minDate}
-                    maxDate={maxDate}
-                    timePicker={timePicker}
-                    calendarClassName={calendarClassName}
-                    dayClassName={dayClassName}
-                    todayClassName={todayClassName}
-                    selectedDayClassName={selectedDayClassName}
-                    disabledDayClassName={disabledDayClassName}
-                  />
-                </Popover.Panel>
-              </Transition>
-            </>
-          )}
-        </>
-      </Popover>
-    </div>
-  );
+					{!readOnly && (
+						<>
+							<Transition.Child
+								as={Fragment}
+								enter="ease-in-out"
+								enterFrom="opacity-0"
+								enterTo="opacity-100"
+								leave="ease-in-out"
+								leaveFrom="opacity-100"
+								leaveTo="opacity-0"
+							>
+								<div className="fixed inset-0 w-screen bg-gray-900 bg-opacity-50" />
+							</Transition.Child>
+							<Transition
+								as={Fragment}
+								enter="transition ease-out duration-200"
+								enterFrom="opacity-0 translate-y-full duration-300"
+								enterTo="opacity-100 h-auto translate-y-0"
+								leave="transition ease-in duration-150"
+								leaveFrom="opacity-100 translate-y-0"
+								leaveTo="opacity-0 translate-y-full duration-300"
+							>
+								<Popover.Panel dir="rtl" className="absolute bottom-0 w-screen z-10 rounded-t-3xl bg-white p-4">
+									<Calendar
+										value={date}
+										changeHandler={handleChange}
+										propsOnChange={onChange}
+										calendar={calendar}
+										locale={locale}
+										format={format}
+										digits={digits}
+										minDate={minDate}
+										maxDate={maxDate}
+										timePicker={timePicker}
+										calendarClassName={calendarClassName}
+										dayClassName={dayClassName}
+										todayClassName={todayClassName}
+										selectedDayClassName={selectedDayClassName}
+										disabledDayClassName={disabledDayClassName}
+									/>
+								</Popover.Panel>
+							</Transition>
+						</>
+					)}
+				</>
+			</Popover>
+		</div>
+	);
 };
 
 export default MobileDatePicker;
