af5ccef0f0cc71aa70905b2ccc4373d29fb48719
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-06-12 13:34:06 +0330

    improved DatePicker styles

--- src/persianDatePickerReact/components/DatePicker/DatePicker.tsx
+++ src/persianDatePickerReact/components/DatePicker/DatePicker.tsx
@@ -25,6 +25,7 @@ const DatePicker: React.FC<DatePickerProps> = ({
 	readOnly = false,
 	inputPlaceholder,
 	inputContainerClassName,
+	calendarIconClassName,
 	calendarClassName,
 	dayClassName,
 	todayClassName,
@@ -42,9 +43,7 @@ const DatePicker: React.FC<DatePickerProps> = ({
 
 	let format = "YYYY/MM/DD HH:mm";
 
-	if (!timePicker) {
-		format = "YYYY/MM/DD";
-	}
+	if (!timePicker) format = "YYYY/MM/DD";
 
 	useEffect(() => {
 		let date = value;
@@ -71,34 +70,32 @@ const DatePicker: React.FC<DatePickerProps> = ({
 
 	return (
 		<div className="w-full font-iranyekan">
-			<Popover className="relative w-full">
+			<Popover className="w-full">
 				<>
 					<Popover.Button
-						className={`outline-none bg-white z-10 h-12 border-1 border-secondary300 rounded-lg pl-2 ${
-							readOnly ? "cursor-not-allowed" : "cursor-pointer focus:border-primary"
+						dir="ltr"
+						className={`flex items-center justify-center h-12 border-1 border-secondary300 rounded-lg outline-none focus:outline-none ${
+							readOnly ? "" : "cursor-pointer focus:border-1 focus:border-primary"
 						} ${inputContainerClassName}`}
 					>
-						<div dir={stringDate ? "ltr" : "rtl"} className="relative flex h-full">
-							<input
-								ref={inputRef}
-								id="date-picker-input"
-								readOnly
-								type="text"
-								value={stringDate ? stringDate : null}
-								data-time-stamp={toTimeStamp(date, timePicker)}
-								placeholder={inputPlaceholder}
-								className={`placeholder:text-secondary400 ${inputPlaceholder ? "pr-16" : ""} ${
-									readOnly ? "cursor-not-allowed" : "cursor-pointer"
-								}`}
-							/>
+						<input
+							dir={stringDate ? "ltr" : "rtl"}
+							ref={inputRef}
+							id="date-picker-input"
+							readOnly
+							type="text"
+							value={stringDate ? stringDate : null}
+							data-time-stamp={toTimeStamp(date, timePicker)}
+							placeholder={inputPlaceholder}
+							className={`h-full px-2 rounded-l-lg bg-white placeholder:text-secondary400 focus:outline-none`}
+						/>
 
-							<label
-								htmlFor="date-picker-input"
-								className="absolute right-0 flex h-full cursor-pointer items-center justify-center rounded-r-lg bg-[#D9D9D9] px-4"
-							>
-								<CalendarIcon className="!fill-[#666666]" />
-							</label>
-						</div>
+						<label
+							htmlFor="date-picker-input"
+							className={`flex h-full items-center justify-center rounded-r-lg bg-[#D9D9D9] px-4 ${calendarIconClassName}`}
+						>
+							<CalendarIcon className="!fill-[#666666]" />
+						</label>
 					</Popover.Button>
 
 					{!readOnly && (
