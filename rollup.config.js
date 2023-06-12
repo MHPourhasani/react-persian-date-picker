590a056177264a24d480acc4fcdc41d8711ad379
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-06-12 13:33:37 +0330

    updated rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,52 +5,56 @@ import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
 import copy from "rollup-plugin-copy";
 import image from "rollup-plugin-image";
+import { babel } from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
 
 const packageJson = require("./package.json");
 
 const obj = {
-  input: "src/index.ts",
-  output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      assetFileNames: "[name][extname]",
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      assetFileNames: "[name][extname]",
-    },
-  ],
-  plugins: [
-    peerDepsExternal({
-      includeDependencies: true,
-    }),
-    resolve({
-      browser: true,
-    }),
-    postcss({
-      config: {
-        path: "./postcss.config.js",
-      },
-      extensions: [".css"],
-      minimize: true,
-      inject: {
-        insertAt: "top",
-      },
-    }),
-    commonjs(),
-    image(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    copy({
-      targets: [
-        {
-          src: "src/persianDatePickerReact/assets",
-          dest: "react-persian-date-picker/persianDatePickerReact",
-        },
-      ],
-    }),
-  ],
+	input: "src/index.ts",
+	output: [
+		{
+			file: packageJson.main,
+			format: "cjs",
+			assetFileNames: "[name][extname]",
+		},
+		{
+			file: packageJson.module,
+			format: "esm",
+			assetFileNames: "[name][extname]",
+		},
+	],
+	plugins: [
+		peerDepsExternal({
+			includeDependencies: true,
+		}),
+		resolve({
+			browser: true,
+		}),
+		postcss({
+			config: {
+				path: "./postcss.config.js",
+			},
+			extensions: [".css"],
+			minimize: true,
+			inject: {
+				insertAt: "top",
+			},
+		}),
+		commonjs(),
+		image(),
+		typescript({ useTsconfigDeclarationDir: true }),
+		copy({
+			targets: [
+				{
+					src: "src/persianDatePickerReact/assets",
+					dest: "react-persian-date-picker/persianDatePickerReact",
+				},
+			],
+		}),
+		babel({ babelHelpers: "bundled" }),
+		terser(),
+	],
 };
 
 export default obj;
