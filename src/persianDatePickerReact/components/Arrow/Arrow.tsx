70b1bb8235929b96fa7a64f4526774ad97cf06ba
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-06-13 08:57:54 +0330

    improved Arrow styles

--- src/persianDatePickerReact/components/Arrow/Arrow.tsx
+++ src/persianDatePickerReact/components/Arrow/Arrow.tsx
@@ -9,22 +9,22 @@ import ArrowUpIcon from "../../assets/svg/ArrowUpIcon";
 import ArrowDownIcon from "../../assets/svg/ArrowDownIcon";
 
 const Arrow: React.FC<ArrowProps> = ({ direction, onClick, disabled }) => {
-  return (
-    <span
-      onClick={onClick}
-      className={`flex h-5 w-5 cursor-pointer items-center justify-center rounded-full hover:bg-gray-100 ${
-        disabled ? "fill-secondary300" : ""
-      }`}
-    >
-      {direction === "left" && <ArrowRightIcon className="rotate-180" />}
+	return (
+		<span
+			onClick={onClick}
+			className={`flex cursor-pointer items-center justify-center rounded-full hover:bg-gray-100 ${
+				disabled ? "fill-secondary300" : ""
+			}`}
+		>
+			{direction === "left" && <ArrowRightIcon className="rotate-180 h-4 w-4" />}
 
-      {direction === "right" && <ArrowRightIcon />}
+			{direction === "right" && <ArrowRightIcon className="h-4 w-4" />}
 
-      {direction === "up" && <ArrowUpIcon />}
+			{direction === "up" && <ArrowUpIcon className="h-6 w-6" />}
 
-      {direction === "down" && <ArrowDownIcon />}
-    </span>
-  );
+			{direction === "down" && <ArrowDownIcon className="h-6 w-6" />}
+		</span>
+	);
 };
 
 export default Arrow;
