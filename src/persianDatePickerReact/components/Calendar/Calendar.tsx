ea7f6753850610fc9c917f43ba76ce59ed53bf92
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-06-27 16:15:36 +0330

    updated Calendar component

--- src/persianDatePickerReact/components/Calendar/Calendar.tsx
+++ src/persianDatePickerReact/components/Calendar/Calendar.tsx
@@ -19,188 +19,166 @@ import { CalendarProps, StateType } from "./CalendarInterface";
 import ClockIcon from "../../assets/svg/ClockIcon";
 
 const Calendar: React.FC<CalendarProps> = ({
-  value,
-  calendar,
-  locale,
-  format,
-  changeHandler,
-  propsOnChange,
-  minDate,
-  maxDate,
-  timePicker,
-  digits,
-  calendarClassName,
-  dayClassName,
-  todayClassName,
-  selectedDayClassName,
-  disabledDayClassName,
+	value,
+	calendar,
+	locale,
+	format,
+	changeHandler,
+	propsOnChange,
+	minDate,
+	maxDate,
+	timePicker,
+	digits,
+	calendarClassName,
+	dayClassName,
+	todayClassName,
+	selectedDayClassName,
+	disabledDayClassName,
 }) => {
-  const [state, setState] = useState<any>({}),
-    [showMonthPicker, setShowMonthPicker] = useState<boolean>(false),
-    [showYearPicker, setShowYearPicker] = useState<boolean>(false),
-    [showTimePicker, setShowTimePicker] = useState<boolean>(false),
-    [yearState, setYearsState] = useState<number>(1397);
+	const [state, setState] = useState<any>({}),
+		[showDayPicker, setShowDayPicker] = useState<boolean>(true),
+		[showMonthPicker, setShowMonthPicker] = useState<boolean>(false),
+		[showYearPicker, setShowYearPicker] = useState<boolean>(false),
+		[showTimePicker, setShowTimePicker] = useState<boolean>(false),
+		[yearState, setYearsState] = useState<number>(1397);
 
-  const weekStartDayIndex = 0,
-    numberOfMonths = 1,
-    onlyShowInRangeDates = true;
+	const weekStartDayIndex = 0,
+		numberOfMonths = 1,
+		onlyShowInRangeDates = true;
 
-  const getDateInRangeOfMinAndMaxDate = (
-    date: any,
-    minDate: any,
-    maxDate: any,
-    calendar: CalendarType
-  ) => {
-    if (minDate)
-      minDate = toDateObject(minDate, calendar).set({
-        hour: 0,
-        minute: 0,
-        second: 0,
-      });
+	const getDateInRangeOfMinAndMaxDate = (date: any, minDate: any, maxDate: any, calendar: CalendarType) => {
+		if (minDate)
+			minDate = toDateObject(minDate, calendar).set({
+				hour: 0,
+				minute: 0,
+				second: 0,
+			});
 
-    if (maxDate)
-      maxDate = toDateObject(maxDate, calendar).set({
-        hour: 23,
-        minute: 59,
-        second: 59,
-      });
+		if (maxDate)
+			maxDate = toDateObject(maxDate, calendar).set({
+				hour: 23,
+				minute: 59,
+				second: 59,
+			});
 
-    return [date, minDate, maxDate];
-  };
+		return [date, minDate, maxDate];
+	};
 
-  const getSelectedDate = (value: any) => {
-    if (!value) return {};
-    if (value instanceof DateObject) return value;
-  };
+	const getSelectedDate = (value: any) => {
+		if (!value) return {};
+		if (value instanceof DateObject) return value;
+	};
 
-  useEffect(() => {
-    setState((state: StateType) => {
-      const { focused } = state;
-      let { date, selectedDate } = state;
+	useEffect(() => {
+		setState((state: StateType) => {
+			const { focused } = state;
+			let { date, selectedDate } = state;
 
-      function getDate(value: any) {
-        return new DateObject(value);
-      }
+			function getDate(value: any) {
+				return new DateObject(value);
+			}
 
-      if (!value) {
-        if (!date) date = getDate({ calendar, locale, format });
-      } else {
-        selectedDate = getSelectedDate(value);
+			if (!value) {
+				if (!date) date = getDate({ calendar, locale, format });
+			} else {
+				selectedDate = getSelectedDate(value);
 
-        if (!date || numberOfMonths === 1) {
-          date = getDate(selectedDate);
-        }
-      }
+				if (!date || numberOfMonths === 1) {
+					date = getDate(selectedDate);
+				}
+			}
 
-      return {
-        ...state,
-        date,
-        selectedDate,
-        value,
-        focused,
-        calendar,
-        locale,
-        format,
-        year: date.year,
-        today: new DateObject({ calendar, locale }),
-      };
-    });
-  }, [value, calendar, locale, format, numberOfMonths, digits]);
+			return {
+				...state,
+				date,
+				selectedDate,
+				value,
+				focused,
+				calendar,
+				locale,
+				format,
+				year: date.year,
+				today: new DateObject({ calendar, locale }),
+			};
+		});
+	}, [value, calendar, locale, format, numberOfMonths, digits]);
 
-  useEffect(() => {
-    if (!minDate && !maxDate) return;
+	useEffect(() => {
+		if (!minDate && !maxDate) return;
 
-    setState((state: StateType) => {
-      const { calendar } = state;
+		setState((state: StateType) => {
+			const { calendar } = state;
 
-      const [selectedDate, $minDate, $maxDate] = getDateInRangeOfMinAndMaxDate(
-        getSelectedDate(value),
-        minDate,
-        maxDate,
-        calendar
-      );
+			const [selectedDate, $minDate, $maxDate] = getDateInRangeOfMinAndMaxDate(getSelectedDate(value), minDate, maxDate, calendar);
 
-      return {
-        ...state,
-        inRangeDates: selectedDate,
-        minDate: $minDate,
-        maxDate: $maxDate,
-      };
-    });
-  }, [minDate, maxDate, onlyShowInRangeDates, value]);
+			return {
+				...state,
+				inRangeDates: selectedDate,
+				minDate: $minDate,
+				maxDate: $maxDate,
+			};
+		});
+	}, [minDate, maxDate, onlyShowInRangeDates, value]);
 
-  const handleChange = (selectedDate: any, state: any) => {
-    changeHandler?.(selectedDate);
-    if (state) setState(state);
-  };
+	const handleChange = (selectedDate: any, state: any) => {
+		changeHandler?.(selectedDate);
+		if (state) setState(state);
+	};
 
-  const globalProps = {
-    state,
-    setState,
-    onChange: handleChange,
-    monthAndYears: getMonthsAndYears(state, numberOfMonths),
-    setShowMonthPicker,
-    setShowYearPicker,
-  };
+	const globalProps = {
+		state,
+		setState,
+		onChange: handleChange,
+		monthAndYears: getMonthsAndYears(state, numberOfMonths),
+		setShowDayPicker,
+		setShowMonthPicker,
+		setShowYearPicker,
+	};
 
-  return (
-    state.today && (
-      <section
-        dir="rtl"
-        className={`flex w-full flex-col items-center shadow-calendar justify-center rounded-lg p-4 bg-white ${calendarClassName}`}
-      >
-        <Header
-          {...globalProps}
-          showMonthPicker={showMonthPicker}
-          showYearPicker={showYearPicker}
-          yearState={yearState}
-          setYearsState={setYearsState}
-        />
+	return (
+		state.today && (
+			<section
+				dir="rtl"
+				className={`flex w-full flex-col items-center shadow-calendar justify-center rounded-lg p-4 bg-white ${calendarClassName}`}
+			>
+				<Header
+					{...globalProps}
+					showDayPicker={showDayPicker}
+					showMonthPicker={showMonthPicker}
+					showYearPicker={showYearPicker}
+					yearState={yearState}
+					setYearsState={setYearsState}
+				/>
 
-        <div className="relative flex w-full flex-col items-center justify-center">
-          <DayPicker
-            {...globalProps}
-            propsOnChange={propsOnChange}
-            timePicker={timePicker}
-            onlyShowInRangeDates={onlyShowInRangeDates}
-            numberOfMonths={numberOfMonths}
-            weekStartDayIndex={weekStartDayIndex}
-            dayClassName={dayClassName}
-            todayClassName={todayClassName}
-            selectedDayClassName={selectedDayClassName}
-            disabledDayClassName={disabledDayClassName}
-          />
+				<div className="relative flex w-full flex-col items-center justify-center">
+					{showDayPicker && (
+						<DayPicker
+							{...globalProps}
+							propsOnChange={propsOnChange}
+							timePicker={timePicker}
+							onlyShowInRangeDates={onlyShowInRangeDates}
+							numberOfMonths={numberOfMonths}
+							weekStartDayIndex={weekStartDayIndex}
+							dayClassName={dayClassName}
+							todayClassName={todayClassName}
+							selectedDayClassName={selectedDayClassName}
+							disabledDayClassName={disabledDayClassName}
+						/>
+					)}
 
-          <div className="absolute top-0 flex w-full flex-col items-center justify-center">
-            {showMonthPicker && <MonthPicker {...globalProps} />}
-          </div>
+					{showMonthPicker && <MonthPicker {...globalProps} />}
+					{showYearPicker && <YearPicker {...globalProps} yearState={yearState} />}
+					<div className="absolute top-0">
+						{showTimePicker && <TimePicker state={state} onChange={handleChange} setShowTimePicker={setShowTimePicker} />}
+					</div>
+				</div>
 
-          <div className="absolute top-0 flex w-full flex-col items-center justify-center">
-            {showYearPicker && (
-              <YearPicker {...globalProps} yearState={yearState} />
-            )}
-          </div>
-
-          {timePicker && (
-            <ClockIcon
-              onClick={() => setShowTimePicker(!showTimePicker)}
-              className="mt-2 cursor-pointer !fill-primary"
-            />
-          )}
-        </div>
-
-        <div className="absolute top-0 flex w-full flex-col items-center justify-center">
-          {showTimePicker && (
-            <TimePicker
-              state={state}
-              onChange={handleChange}
-              setShowTimePicker={setShowTimePicker}
-            />
-          )}
-        </div>
-      </section>
-    )
-  );
+				{timePicker && (
+					<ClockIcon onClick={() => setShowTimePicker(!showTimePicker)} className="mt-2.5 cursor-pointer !fill-primary" />
+				)}
+			</section>
+		)
+	);
 };
 
 export default Calendar;
