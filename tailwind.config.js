5ae5e4a6c3634c803c0e52d777cf410e7a602f02
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-07-22 16:19:48 +0330

    add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/persianDatePickerReact/components/**/*.{js,ts,jsx,tsx}",
-    "./src/persianDatePickerReact/assets/svg/**/*.{js,ts,jsx,tsx}",
+    content: [
+    "./src/reactTags/components/**/*.{js,ts,jsx,tsx}",
+    "./src/reactTags/assets/icons/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        iranyekan: ["iranyekan"],
-      },
-      fontSize: {
-        11: "11px",
-        14: "14px",
-        15: "15px",
-        16: "16px",
-      },
-      colors: {
-        white: "#fff",
-        primary: "#3880FF",
-        secondary300: "#D9DBE9",
-        secondary400: "#A0A3BD",
-        secondary600: "#4E4B66",
-        secondary800: "#14142A",
-      },
-      borderWidth: { 1: "1px", 1.5: "1.5px" },
-      boxShadow: {
-        calendar: "-4px 4px 16px rgba(0, 0, 0, 0.08)",
-      },
+    theme: {
+        extend: {
+            fontFamily: {
+                iranyekan: ['iranyekan'],
+            },
+            colors: {
+                bg: {
+                    dark: '#1C1B1E',
+                    light: '#FFF',
+                },
+                secondary: {
+                    10: '#EFF0F6',
+                    100: '#DBDDDE',
+                    400: '#A0A3BD',
+                },
+                zGray: {
+                    300: '#CDCDCD',
+                    700: '#222831',
+                    800: '#151515',
+                },
+                zDark: {
+                    5: '#E3E2E6',
+                    12: '#EFF0F6',
+                },
+            },
+            maxWidth: {
+                container: '774px',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
