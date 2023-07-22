0432a3839010436b31341b5b591f34a08dff9743
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-07-22 16:20:33 +0330

    add general interfaces

---
+++ src/reactTags/interfaces/general.ts
@@ -0,0 +1,6 @@
+export type ThemeType = 'dark' | 'light';
+
+export type ModeType = 'array-of-string' | 'multi-select' | 'advanced-multi-select';
+
+export type SelectedTagsType = any[];
+export type FilteredTagsType = any[];
