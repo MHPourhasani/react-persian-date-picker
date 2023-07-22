411f2da3c1ba85f0cdf0aa3d92012cfd926dcc0d
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-07-22 16:21:18 +0330

    export ReactTags component

---
+++ src/reactTags/main.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../index.css";
+import { TagContainerProps } from "./components/TagContainer/TagContainer.interface";
+import TagContainer from "./components/TagContainer/TagContainer";
+
+const ReactTags = (props: TagContainerProps) => {
+  return (
+    <TagContainer
+      mode={props.mode}
+      theme={props.theme}
+      maxTags={props.maxTags}
+      tagsList={props.tagsList}
+      title={props.title}
+      inputPlaceholder={props.inputPlaceholder}
+      inputClassName={props.inputClassName}
+      addToCategoryOnClick={props.addToCategoryOnClick}
+      dropDownContainerClassName={props.dropDownContainerClassName}
+    />
+  );
+};
+
+export default ReactTags
