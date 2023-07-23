a7cbe20e8d89172dbbbe638ce52e7aa25e072267
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-07-23 17:03:28 +0330

    add id to Input component

--- src/reactTags/components/Input/Input.tsx
+++ src/reactTags/components/Input/Input.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 import { InputProps } from './Input.interface';
 
 const Input = ({ theme, placeholder, setShowDropdown, value, changeHandler, keyDown, inputClassName }: InputProps) => {
-    const inputMouseDown = (e: any) => {
-        if (!document.getElementById('input')?.contains(e.target)) {
-            // setShowDropdown(false);
-        }
-    };
-
     return (
         <input
+            id="input"
             type="text"
             dir="rtl"
             placeholder={placeholder}
@@ -18,7 +13,6 @@ const Input = ({ theme, placeholder, setShowDropdown, value, changeHandler, keyD
             onChange={changeHandler}
             onKeyDown={keyDown}
             onFocus={() => setShowDropdown(true)}
-            onMouseDown={inputMouseDown}
             className={`w-60 py-1 px-4 rounded-[10px] outline-none placeholder:text-right bg-transparent placeholder:text-sm placeholder:text-secondary-400 placeholder:opacity-0 focus:placeholder:opacity-100 transition-all ease-in-out ${
                 theme === 'dark' ? 'text-white' : 'text-zGray-800'
             } ${inputClassName}`}
