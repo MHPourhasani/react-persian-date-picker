7aa6d727ae658e7ad9f225d83f7f981b71c79d5c
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-07-23 17:48:11 +0330

    improved placeholder styles

--- src/reactTags/components/Input/Input.tsx
+++ src/reactTags/components/Input/Input.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { InputProps } from './Input.interface';
 
-const Input = ({ theme, placeholder, setShowDropdown, value, changeHandler, keyDown, inputClassName }: InputProps) => {
+const Input = ({ theme, placeholder, setShowDropdown, value, changeHandler, keyDown, inputClassName, selectedTags }: InputProps) => {
     return (
         <input
             id="input"
@@ -13,9 +13,9 @@ const Input = ({ theme, placeholder, setShowDropdown, value, changeHandler, keyD
             onChange={changeHandler}
             onKeyDown={keyDown}
             onFocus={() => setShowDropdown(true)}
-            className={`w-60 py-1 px-4 rounded-[10px] outline-none placeholder:text-right bg-transparent placeholder:text-sm placeholder:text-secondary-400 placeholder:opacity-0 focus:placeholder:opacity-100 transition-all ease-in-out ${
-                theme === 'dark' ? 'text-white' : 'text-zGray-800'
-            } ${inputClassName}`}
+            className={`w-60 py-1 px-4 rounded-[10px] outline-none placeholder:text-right bg-transparent placeholder:text-sm placeholder:text-secondary-400 ${
+                selectedTags.length ? 'placeholder:opacity-0' : 'placeholder:opacity-100'
+            } focus:placeholder:opacity-100 transition-all ease-in-out ${theme === 'dark' ? 'text-white' : 'text-zGray-800'} ${inputClassName}`}
         />
     );
 };
