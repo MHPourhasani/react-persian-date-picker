cb3e3ffce8a33cb5d3bdede592def8824ba5578d
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-07-26 11:52:04 +0330

    updated EmptyList component

--- src/reactTags/components/EmptyList/EmptyList.tsx
+++ src/reactTags/components/EmptyList/EmptyList.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import { EmptyListProps } from './EmptyList.interface';
 
-const EmptyList = ({ theme, filteredTags, clickHandler, inputValue }: EmptyListProps) => {
+const EmptyList = ({ theme, clickHandler, inputValue }: EmptyListProps) => {
     return (
-        <div className="absolute w-full bg-white rounded-xl">
-            {!filteredTags.length && (
-                <div className={`w-full flex-col flex`}>
-                    <button
-                        onClick={clickHandler}
-                        className={`flex gap-2 cursor-pointer rounded-lg px-2 py-[7px] ${
-                            theme === 'dark' ? 'text-white hover:bg-zGray-700' : 'text-zGray-800 hover:bg-zDark-12'
-                        }`}
-                    >
-                        <span>افزودن</span>
-                        <span>"{inputValue}"</span>
-                        <span>به دسته بندی ها</span>
-                    </button>
-                </div>
-            )}
+        <div className=" w-full bg-white rounded-xl">
+            <div className={`w-full flex-col flex`}>
+                <button
+                    onClick={clickHandler}
+                    className={`flex gap-2 cursor-pointer rounded-lg px-2 py-[7px] ${
+                        theme === 'dark' ? 'text-white hover:bg-zGray-700' : 'text-zGray-800 hover:bg-zDark-12'
+                    }`}
+                >
+                    <span>افزودن</span>
+                    <span>"{inputValue}"</span>
+                </button>
+            </div>
         </div>
     );
 };
